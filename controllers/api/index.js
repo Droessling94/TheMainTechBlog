49a25199c5e3161fd1c285fb4b4a291c2f70c551
Droessling94 <101940633+Droessling94@users.noreply.github.com>
2022-05-09 21:12:52 -0400

    more work on the routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,12 @@
-const { route } = require("../../../.Main/controllers/api");
+
 const userRoutes = require('./userRoutes')
 const postRoutes = require('./postRoutes')
 const commentRoutes = require('./commentRoutes');
+const withAuth = require("../../utils/auth");
 
 const router = require("express").Router();
+
+router.use("/user", );
 
 
 router.use("/user", userRoutes);
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,9 +2,8 @@ const router = require("express").Router();
 const session = require("express-session");
 const { User } = require("../../models");
 
-
+//========CREATE A NEW USER========//
 router.post("/", async (req, res) => {
-    // Creating a new instance of user
     try {
         const userData = await User.create(req.body);
         res.status(200).json(userData);
@@ -13,8 +12,9 @@ router.post("/", async (req, res) => {
       }
 });
 
+//========LOGIN ROUTE========//
 router.post("/login", async (req, res) => {
-    // User login
+    //========CHECK USER CREDNETIALS AND SET NEW SESSION========//
     try{
         if(req.body.username == username && req.body.password == userpassword){
             session = req.session;
@@ -28,6 +28,7 @@ router.post("/login", async (req, res) => {
 
 });
 
+//========LOGOUT DELETES SESSION========//
 router.get("/logout", async (req, res) => {
     req.session.destroy();
     res.redirect('login')
